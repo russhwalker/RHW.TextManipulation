6cdf803c8bfc8d2ef9a5ae1133aad1798deae3c2
Russell Walker <russ.h.walker@gmail.com>
2015-05-19 22:46:32 -0400

    redoing it all

---
+++ src/RHW.TextManipulation/wwwroot/lib/textFunctions.ts
@@ -0,0 +1,40 @@
+﻿
+enum RuleType { Remove, Replace }
+
+
+interface RuleParams {
+}
+
+class TextReplaceParams implements RuleParams {
+    searchValue: string;
+    replaceValue: string;
+}
+
+class TextRule {
+    inputText: string;
+    responseText: string;
+    ruleType: RuleType;
+    ruleParams: RuleParams;
+
+    constructor(inputText: string, ruleType: RuleType, ruleParams: RuleParams) {
+        this.inputText = inputText;
+        this.ruleType = ruleType;
+        this.ruleParams = ruleParams;
+    }
+
+    apply() {
+        switch (this.ruleType) {
+            case RuleType.Remove:
+                this.responseText = this.inputText.replace('abc', '');
+                break;
+        }
+    }
+
+}
+
+class RuleSet {
+    textRules: TextRule[];
+    constructor(textRules: TextRule[]) {
+
+    }
+}
